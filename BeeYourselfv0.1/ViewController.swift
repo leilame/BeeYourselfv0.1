3672ba098a75e5700ecea4fe3a499597a87ab34f
Ozay Demirezen <demirezen@gmail.com>
2019-08-19 16:16:50 +0100

    Initial Commit

---
+++ BeeYourselfv0.1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeeYourselfv0.1
+//
+//  Created by Ozay Demirezen on 19/08/2019.
+//  Copyright © 2019 Emad Heydari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BeeYourselfv0.1Tests/BeeYourselfv0_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  BeeYourselfv0_1Tests.swift
+//  BeeYourselfv0.1Tests
+//
+//  Created by Ozay Demirezen on 19/08/2019.
+//  Copyright © 2019 Emad Heydari. All rights reserved.
+//
+
+import XCTest
+@testable import BeeYourselfv0_1
+
+class BeeYourselfv0_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
